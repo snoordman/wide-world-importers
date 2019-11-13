ac8fac24ebf5e29a71fb957ff5c00924092934dc
Robin Donkers <robindonkers01@gmail.com>
2019-11-13 11:57:02 +0100

    no message

--- productpagina.php
+++ productpagina.php
@@ -13,19 +13,11 @@
 <body>
 
 <div class="header">
-<<<<<<< HEAD
-    <img src="WWI-logo.png" height="45" width="125" alt="logo"><br>
+    <img src="afbeeldingen/WWI-logo.png" class='logo' alt="Afbeelding">
     <ul>
         <li><a href="">Producten</a></li>
     </ul>
-
-=======
-    <img src="afbeeldingen/WWI-logo.png" height="45" width="125" alt="logo">
 </div>
->>>>>>> 6b938932e3aec848b737a6d69e5fdf90383c1e1c
-
-</div>
-
 
 </body>
 </html>
