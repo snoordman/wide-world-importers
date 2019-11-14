9c8b3f01d75c6b407fd2472c1af3b8e55660f653
Eren Demirhan <s1147295@student.windesheim.nl>
2019-11-14 16:19:10 +0100

    no message

--- viewFile/contactpagina.php
+++ viewFile/contactpagina.php
@@ -1,17 +1,47 @@
 
 
+    <div class="formuliernaam">
+        <h1>Contactformulier &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp   </h1>
+    </div>
+
+
+
+
+
+
+
+<div class="gegevensnaam">
+<h1>Onze bedrijfsgegevens</h1>
+</div>
+
+
+<div class="adres">
+    Adres<br>
+    Wide World Importers<br>
+    Donerkebapstraat 80<br>
+    8077TH Zwolle<br>
+    Tel: 038-4448392<br>
+    E-mail: info@WWI.nl<br>
+    KVK: 85739857398<br>
+</div>
+
 
 <div class="formulier">
-<h1>Contactformulier</h1>
-
     Uw naam:
-    <br><input type="text" name="naam"><br>
+    <br><input type="text" class="formulier" name="naam"><br>
     Uw e-mail adres:
-    <br><input type="email" name="email"><br>
+    <br><input type="email" class="formulier" name="email"><br>
     Uw vraag:
-    <br><textarea name="message" row="4" cols="22"></textarea><br>
+    <br><textarea name="message" row="4" class="formulier" cols="22"></textarea><br>
     <input type="submit" value="verzenden"><br>
 
 
 
 </div>
+
+
+
+
+
+
+
