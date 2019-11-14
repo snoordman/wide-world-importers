f583d068e8cc1dbe3aa45cdc363dedda7e9ee195
Eren Demirhan <s1147295@student.windesheim.nl>
2019-11-14 14:35:19 +0100

    no message

--- viewFile/contactpagina.php
+++ viewFile/contactpagina.php
@@ -1,5 +1,13 @@
 <div>
-<h1>Contact</h1>
+<h1>Contactformulier</h1>
+
+    Uw naam:
+    <br><input type="text" name="naam"><br>
+    Uw e-mail adres:
+    <br><input type="email" name="email"><br>
+    Uw vraag:
+    <br><textarea name="message" row="4" cols="22"></textarea>
+
 
 
 </div>
