a1b9019312f815fa566d2177e2ab298e19c40e4e
Robin Donkers <robindonkers01@gmail.com>
2019-11-19 20:12:00 +0100

    no message

--- productpagina.php
+++ productpagina.php
@@ -1,3 +1,5 @@
+//Volgens mij doet deze pagina niks
+
 <?php
     $viewFile = "viewFile/productpagina.php";
     require_once "template.php";
--- viewFile/productpagina.php
+++ viewFile/productpagina.php
@@ -1,3 +1,5 @@
+//Volgens mij doet deze pagina niks
+
 <a href="productpagina.php">klik hier voor productpagina</a>
 
 <form method="get" action="index.php">
