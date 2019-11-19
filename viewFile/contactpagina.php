4688fde01acc05fe164c265a3e5d845116fe80ed
Eren Demirhan <s1147295@student.windesheim.nl>
2019-11-19 14:00:58 +0100

    contact pagina bijgewerkt

--- viewFile/contactpagina.php
+++ viewFile/contactpagina.php
@@ -48,7 +48,7 @@
             <img src="foto3.jpg" class="linkedin" alt="linkedin">
         </div>
         <div class="col-3">
-            <br>
+
             <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&height=600&hl=nl&q=Stationspassage%205%2C%208017%20JR%20Zwolle+(U.yildiz%20doner%20company)&ie=UTF8&t=&z=16&iwloc=A&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.mapsdirections.info/nl/maak-een-google-map/">Maak een Google Map</a> van <a href="https://www.mapsdirections.info/nl/">Nederland Kaart</a></iframe>
             </div><br /></div>
     </div>
