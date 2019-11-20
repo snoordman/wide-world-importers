bedde66834895ab6985cc9660c19062cf9c04688
Eren Demirhan <s1147295@student.windesheim.nl>
2019-11-20 11:25:21 +0100

    no message

--- contactpagina.php
+++ contactpagina.php
@@ -1,6 +1,22 @@
 <?php
-    $viewFile = "viewFile/contactpagina.php";
+$viewFile = "viewFile/contactpagina.php";
+if(isset($_POST["submit"])){
+    $naam=$_POST["naam"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+$to='eren-demirhan@live.nl';
+$subject='form submission';
+$message="Name:" . $naam. "\n" . "email:" . $email . "\n" . "heeft het volgende bericht gestuurd:" . "\n" . "\n" . $message;
+$headers="From " . $email;
 
+if(mail($to, $subject, $message, $headers)){
+echo "<h1>Mail is verzonden!" . $naam . ", U krijgt zo snel mogelijk te horen van ons</h1>" ;
+
+} else {
+  echo "Er is iets mis gegaan";
+}
+
+}
 
 
 
