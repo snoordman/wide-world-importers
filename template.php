7a47b74856c5f294cf3792b20ac5af1a49ccce7c
Robin Donkers <robindonkers01@gmail.com>
2019-11-20 10:03:36 +0100

    Kleine aanpassingen

--- template.php
+++ template.php
@@ -40,6 +40,7 @@
                     <li class="nav-item"><a class="nav-link" href="browseproduct.php">Producten</a></li>
                     <li class="nav-item"><a class="nav-link" href="contactpagina.php">Contact</a></li>
                     <li class="nav-item"><a class="nav-link" href="faqpagina.php">FAQ</a></li>
+                    <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-sign-in-alt"></i></a></li>
                 </ul>
                         <form class="form-inline my-2 my-lg-0" method="post" action="browseproduct.php">
                             <input class="form-control mr-sm-2" type="search" placeholder="Zoeken..">
