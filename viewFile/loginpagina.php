b55e98fe115e6904fa3fbb5b5967a9377affb8c1
Robin Donkers <robindonkers01@gmail.com>
2019-11-20 10:58:03 +0100

    Login pagina gemaakt

---
+++ viewFile/loginpagina.php
@@ -0,0 +1,22 @@
+<div class="login-form">
+    <form action="/examples/actions/confirmation.php" method="post">
+        <h1 class="text-center">Inloggen</h1>
+        <div class="form-group">
+            <input type="text" class="login-form-control" placeholder="E-mailadres" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="login-form-control" placeholder="Wachtwoord" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="loginbutton loginbutton-primary loginbutton-block">Inloggen</button>
+        </div>
+        <div class="text-center">
+            <a href="#">Wachtwoord vergeten?</a>
+        </div>
+        <div class="form-group">
+            <br><br>
+            <h4 class="text-center">Nog geen account?</h4>
+            <button type="submit" class="loginbutton loginbutton-primary loginbutton-block">Registreer hier!</button>
+        </div>
+    </form>
+</div>
