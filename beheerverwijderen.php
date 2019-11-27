76f5fd54ca97800221f658608b1b87eb0a7a268e
Eren Demirhan <s1147295@student.windesheim.nl>
2019-11-27 15:31:02 +0100

    nieuwe query toegevoegd voor verwijderen

--- beheerverwijderen.php
+++ beheerverwijderen.php
@@ -1,24 +1,24 @@
 <?php
 $viewFile = "viewFile/beheerverwijderen.php";
+require_once "functions/products.php";
+require_once "template.php";
 
 if(isset($_GET["submit"])){
 $StockItemID=$_GET["StockItemID"];
 
 $conn = createConn();
-$query= $conn ->prepare("DELETE FROM stockitems_archive WHERE StockItemID = ?");
-$query->blind_param("i", $StockItemID);
+$query= $conn ->prepare("SELECT * INTO stockitems_archive FROM stockitems WHERE StockItemID = ?");
+$query->bind_param("i", $StockItemID);
 $query->execute();
 $conn->close();
 
 
-    
+
 }
 
 
 
 
 
-
-require_once "template.php";
 
 ?>
