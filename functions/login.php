187bf68b7071739d961e9e8fe95760532f088c85
LAPTOP-MV1MBAHT\Stan <sb.noordman@gmail.com>
2019-11-28 10:07:06 +0100

    bezig met alerts

--- functions/login.php
+++ functions/login.php
@@ -11,10 +11,11 @@ function login($email, $password){
         $_SESSION["loggedIn"] = true;
         $_SESSION["permissions"] = ["isSystemUser" => $login["IsSystemUser"], "isEmployee" => $login["IsEmployee"], "isSalesPerson" => $login["IsSalesPerson"]];
         $alertMessage = "U bent succesvol ingelogd";
-        alert_msg_push($successAlert, $alertMessage);
+        alert_msg_push("alert-success", $alertMessage);
         header("Location: index.php");
         exit;
     }else{
-        alert_msg_push($dangerAlert, $alertMessage);
+        $alertMessage = "Verkeerd wachtwoord of email. Probeer alstublieft opnieuw.";
+        alert_msg_push("alert-danger", $alertMessage);
     }
 }
