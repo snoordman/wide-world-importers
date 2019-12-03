0af3eca872d0faf2ed3ff565c07318b4316c2b5e
LAPTOP-MV1MBAHT\Stan <sb.noordman@gmail.com>
2019-12-03 14:05:47 +0100

    bezig met registratie en typfout verbeterd in query

--- functions/sql.php
+++ functions/sql.php
@@ -43,7 +43,7 @@
         $conn = createConn();
 
         $query = $conn->prepare("
-            SELECT  si.StockItemId, si.StockItemName, si.SupplierID, si.ColorID, si.UnitPackageId, si.RecommendedRetailPrice, sh.QuantityOnHand, c.ColorName, si.Size, isChillerStock, Brand, LeadTimeDays
+            SELECT  si.StockItemId, si.StockItemName, si.SupplierID, si.ColorID, si.UnitPackageID, si.RecommendedRetailPrice, sh.QuantityOnHand, c.ColorName, si.Size, isChillerStock, Brand, LeadTimeDays
             FROM    stockitems AS si 
             JOIN    stockitemholdings AS sh ON sh.StockItemId = si.StockItemId
             JOIN    stockitemstockgroups AS sisg ON sisg.StockItemID = si.StockItemID
@@ -243,7 +243,7 @@
         }
     }
 
-    function addUser($firstName, $lastName, $password, $email, $phoneNumber, $userId, $deliveryMethod, $deliveryLocation, $postalLocation, $permissions = null){
+    function addUser($firstName, $lastName, $password, $email, $phoneNumber, $userId, $deliveryMethod, $deliveryLocation, $permissions = null){
         $conn = createConn();
 
         $fullName = $firstName . " " . $lastName;
@@ -251,14 +251,12 @@
         $logonName = $email;
 
         $deliveryCityId = $deliveryLocation[0];
-        $deliveryAddressLine1 = $deliveryLocation[1];
         $deliveryAddressLine2 = $deliveryLocation[2];
         $deliveryPostalCode = $deliveryLocation[3];
 
-        $postalCityId = $postalLocation[0];
-        $postalAddressLine1 = $postalLocation[0];
-        $postalAddressLine2 = $postalLocation[0];
-        $postalPostalCode = $postalLocation[0];
+        $postalCityId = $deliveryLocation[0];
+        $postalAddressLine2 = $deliveryLocation[1];
+        $postalPostalCode = $deliveryLocation[2];
 
         if($password !== false){
             $isSystemUser = 0;
@@ -297,16 +295,16 @@
                 VALUES(($maxIdPeople) + 1, ?, ?, ?, 1, ?, 0, ?, ?, ?, ?, ?, ?, ?, '".date('Y-m-d H:i:s')."' , '9999-12-31 23:59:59');
                 
                 INSERT INTO customers(CustomerId, CustomerName, BillToCustomerId, CustomerCategoryId, PrimaryContactPersonId, DeliveryMethodId, 
-                DeliveryCityId, PostalCityId, AccountOpendDate, StandardDiscountPercentage, PhoneNumber, DeliveryAddressLine1, 
-                DeliveryAddressLine2, DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2, PostalPostalCode)
-                VALUES(($maxIdCustomer), ?, ($maxIdPeople + 1), 9, ($maxIdPeople + 1), ?, ?, ?, '".date('Y-m-d H:i:s') . "', 0.000, ?, ?, ?, ?, ?, ?, ?, ?);
+                DeliveryCityId, PostalCityId, AccountOpendDate, StandardDiscountPercentage, PhoneNumber, 
+                DeliveryAddressLine2, DeliveryPostalCode, DeliveryLocation, PostalAddressLine2, PostalPostalCode, LastEditedBy)
+                VALUES(($maxIdCustomer), ?, ($maxIdPeople + 1), 9, ($maxIdPeople + 1), ?, ?, ?, '".date('Y-m-d H:i:s') . "', 0.000, ?, ?, ?, ?, ?, ?, ?);
             ");
 
-            $query->bind_param("sssssssiiiss
-                                      siiissssssssss
+            $query->bind_param("sssssssiiisi
+                                      siiisssssssss
            ",
                 $fullName, $firstName, $searchName, $logonName, $password, $isSystemUser, $isEmployee, $isSalesperson, $phoneNumber, $email, $userId,
-                $fullName, $deliveryMethod, $deliveryCityId, $postalCityId, $phoneNumber, $deliveryAddressLine1, $deliveryAddressLine2, $deliveryPostalCode, $postalAddressLine1, $postalAddressLine2, $postalPostalCode
+                $fullName, $deliveryMethod, $deliveryCityId, $postalCityId, $phoneNumber, $deliveryAddressLine2, $deliveryPostalCode, $postalAddressLine2, $postalPostalCode, $userId
             );
 
             $result = $query->execute();
