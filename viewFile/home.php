c501927433489648ce6987d6e77f66f1f9d6b48f
Liam van Bart <liamjl@hotmail.nl>
2019-12-17 17:11:14 +0100

    Homepagina neef

--- viewFile/home.php
+++ viewFile/home.php
@@ -1,5 +1,7 @@
 <h1>Onze meest populaire producten:</h1>
 
+
+
 <?php
 
 displayMostPopulairItems();
@@ -9,7 +11,30 @@ displayMostPopulairItems();
 
 ?>
 
+<br>
+<br>
+<div class="row">
+<div class="col-6">
+<div  class="homepagina" >
+
+    <img src="afbeeldingen/homebanner.jpg" alt="FAQ" style="width:550px;height:240px;">
+
+</div>
+</div>
+<div class="col-4">
+    <div class="homepagina1" type="text">
+        <h1>Over WWI</h1>
+        <div type="text1">
+        Wide World Importers (WWI) is een groothandelsimporteur voor nieuwe goederen en distributeur die opereert vanuit de baai van San Francisco.
+        <br>
+        Als groothandel zijn de klanten van WWI meestal bedrijven die aan particulieren doorverkopen. WWI verkoopt aan particuliere klanten in de Verenigde Staten, waaronder speciaalzaken, supermarkten, computerwinkels, winkels voor toeristische attracties en sommige particulieren. WWI verkoopt ook aan andere groothandelaren via een netwerk van agenten die de producten namens WWI promoten. Hoewel alle klanten van WWI momenteel in de Verenigde Staten zijn gevestigd, is het bedrijf van plan uitbreiding naar andere landen te stimuleren.
+        <br>
+        WWI koopt goederen van leveranciers, waaronder fabrikanten van nieuwigheden en speelgoed, en andere groothandelaren in nieuwigheden. Ze slaan de goederen op in hun WWI-magazijn en bestellen zo nodig bij leveranciers om orders van klanten uit te voeren. Ze kopen ook grote hoeveelheden verpakkingsmateriaal in en verkopen deze in kleinere hoeveelheden voor het gemak van de klanten.
+        </div>
 
 
 
 
+    </div>
+</div>
+</div>
