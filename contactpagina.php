985ce71229b8be1dc81336551f64b5881293afb2
LAPTOP-MV1MBAHT\Stan <sb.noordman@gmail.com>
2019-12-19 11:31:24 +0100

    bezig met email

--- contactpagina.php
+++ contactpagina.php
@@ -1,4 +1,6 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require_once "config.php";
     $viewFile = "viewFile/contactpagina.php";
@@ -9,45 +11,59 @@ require_once "config.php";
     $result="";
     $resultfail="";
     if(isset($_POST["submit"])) {
-        require "PHPMailer/PHPMailer/PHPMailerAutoload.php";
+        require 'PHPMailer/PHPMailer/Exception.php';
+        require 'PHPMailer/PHPMailer/PHPMailer.php';
+        require 'PHPMailer/PHPMailer/SMTP.php';
+
+
         $mail = new PHPMailer(true);
 
         $mail->isSMTP();
         $mail->SMTPDebug = 2;
-        $mail->Host = "smtp.gmail.com";
-        $mail->SMTPAuth = true;
 
-        $mail->Port = 587;
-        $mail->SMTPSecure = "tls";
+        $mail->Host = gethostbyname('smtp.gmail.com');
+        $mail->SMTPAuth   = true;
+
         $mail->Username = "erendemirhan66@gmail.com";
         $mail->Password = "TestTest123";
 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+        $mail->Port = 587;
+
+
         try {
             $mail->setFrom($_POST["email"], $_POST["naam"], $_POST["typevraag"]);
-        } catch (phpmailerException $e) {
-            var_dump($e);
-            exit;
+        } catch (Exception $e) {
+            $sendMail = false;
         }
-        $mail->addAddress("erendemirhan66@gmail.com");
+
+
+        try {
+            $mail->addAddress("erendemirhan66@gmail.com");
+        } catch (Exception $e) {
+            $sendMail = false;
+        }
+
+
         $mail->isHTML(true);
         $mail->Subject = "Vraag";
         $mail->Body = '<h1 align=center>naam :' . $_POST['naam'] . '<br>Email: ' . $_POST["email"] . '<br>Type vraag: ' . $_POST["typevraag"] . '<br
         >Message: ' . $_POST["message"] . '</h1>';
+
+
         try {
-            if ($mail->send()) {
-                $result = "Bedankt! " . $_POST["naam"] . " We zullen uw vraag zo spoedig mogelijk beantwoorden";
-
-            } else {
-                $resultfail = "Er is iets fout gegaan, probeer het opnieuw.";
-
-            }
-        } catch (phpmailerException $e) {
-            echo "Error 2";
-            echo "<br />";
-            echo "<br />";
-            echo "<br />";
-            var_dump($e);
-            exit;
+            $sendMail = $mail->send();
+        } catch (Exception $e) {
+            $sendMail = false;
         }
+        if ($sendMail) {
+            $result = "Bedankt! " . $_POST["naam"] . " We zullen uw vraag zo spoedig mogelijk beantwoorden";
+
+        } else {
+            $resultfail = "Er is iets fout gegaan, probeer het opnieuw.";
+
+        }
+
     }
         require_once "template.php";
